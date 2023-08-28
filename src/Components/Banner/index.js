99be3683ea3427f22c1d9b598b53fd18f8c1076a
Bruno Paiva Souza <bruno.battlen@hotmail.com>
2023-08-28 06:48:37 -0300

    Primeira Atualização

---
+++ src/Components/Banner/index.js
@@ -0,0 +1,15 @@
+import './Banner.css'
+
+const Banner = () => {
+
+    return (
+        <>
+            <div className='banner'>
+                <img src='/images/tasks.png' alt='Logotipo'/>
+            </div>
+            <h2>ToDo</h2>
+        </>
+    )
+}
+
+export default Banner;
